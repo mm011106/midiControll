b6a023ca3f8ee826e2d94f0e1164137bd6ebf3fb
mm011106 <mm011106@gmail.com>
2020-07-15 11:00:39 +0900

    add parameter read process in initializing precess

--- FllPad.py
+++ FllPad.py
@@ -364,6 +364,10 @@ if __name__=='__main__':
 	except indexError:
 		raise(IOError("Input port not found."))
 
+	try:
+		with open('para.txt', mode='r') as f:
+			json.load(squid_parameter,f)
+
 	while True:
 		time.sleep(1)
 		# print(measure_distance(xypad_begin_at,xypad_currentry_at))
